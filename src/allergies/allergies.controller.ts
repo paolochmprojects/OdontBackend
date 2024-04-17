473c37f749795a11ebf0137c4785958cf88b3b52
paolochmprojects <paolo.dev.projects@gmail.com>
2024-04-17 16:07:00 -0500

    [UPD] relocation decorator

--- src/allergies/allergies.controller.ts
+++ src/allergies/allergies.controller.ts
@@ -9,18 +9,18 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common';
-import { AllergiesService } from './allergies.service';
-import { JwtAuthGuard } from 'src/auth/guards';
-import { GetUser } from 'src/auth/decorators';
 import { Users } from '@prisma/client';
+import { GetUser } from 'src/auth/decorators';
+import { JwtAuthGuard } from 'src/auth/guards';
+import { AllergiesService } from './allergies.service';
 import { CreateAllergyDto, UpdateAllergyDto } from './dto';
 
+@UseGuards(JwtAuthGuard)
 @Controller('allergies')
 export class AllergiesController {
   constructor(private readonly allergiesService: AllergiesService) {}
 
   @Post()
-  @UseGuards(JwtAuthGuard)
   async create(
     @GetUser() user: Users,
     @Body() createAllergyDto: CreateAllergyDto,
@@ -29,7 +29,6 @@ export class AllergiesController {
   }
 
   @Get()
-  @UseGuards(JwtAuthGuard)
   async findAll(@GetUser() user: Users) {
     return await this.allergiesService.getAllAllergies(user.id);
   }
@@ -37,13 +36,11 @@ export class AllergiesController {
   // TODO: implement search functions
 
   @Delete(':id')
-  @UseGuards(JwtAuthGuard)
   async delete(@GetUser() user: Users, @Param('id', ParseUUIDPipe) id: string) {
     return await this.allergiesService.delete(id, user.id);
   }
 
   @Patch(':id')
-  @UseGuards(JwtAuthGuard)
   async update(
     @GetUser() user: Users,
     @Param('id', ParseUUIDPipe) id: string,
